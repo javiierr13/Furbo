3c55e4128e16cca3b26b78bbdd5fca92ba835254
jessuss00 <jesusmadro23@gmail.com>
2025-03-04 10:21:42 +0100

    atributos defeensa

--- src/equipo/jugador/Jugador.java
+++ src/equipo/jugador/Jugador.java
@@ -1,5 +1,7 @@
 package equipo.jugador;
 
 public class Jugador {
-
+	private String nombre;
+	private int dorsal;
+	private String Equipo;
 }
---
+++ src/equipo/jugador/defensa/Defensa.java
@@ -0,0 +1,8 @@
+package equipo.jugador.defensa;
+
+import equipo.jugador.Jugador;
+
+public class Defensa extends Jugador {
+	private int disputasRealizadas;
+
+}
